a412527cb11c38285a74f903c87fd0b7e2f838c5
ElDiego5599 <angaritadiego37@gmail.com>
2026-06-20 20:57:02 -0500

    fix: align storybook packages to v8.6.14 + .npmrc

---
+++ storybook-static/assets/preview-BI39Njpw.js
@@ -0,0 +1,1 @@
+var e={parameters:{controls:{matchers:{color:/(background|color)$/i,date:/Date$/i}},backgrounds:{default:`dark`,values:[{name:`dark`,value:`#000000`},{name:`light`,value:`#f4f4f5`}]}}};export{e as default};
