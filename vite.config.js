40bac809638269e31e1e2ce697ec28da7fe29bf1
ElDiego5599 <angaritadiego37@gmail.com>
2026-07-01 12:52:01 -0500

    token para reestablecer contrasena y pantallas para cambiarla

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 import { fileURLToPath } from 'url'
@@ -15,7 +14,6 @@ const isAnalyze = process.env.ANALYZE === 'true'
 export default defineConfig({
   plugins: [
     react(),
-    basicSsl(),
     isAnalyze && visualizer({
       filename: 'dist/stats.html',
       open: true,
